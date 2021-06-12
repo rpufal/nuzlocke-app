0aecae608ebae886e413537ee791dc4c703871bb
Rafael Pufal <rafael.pufal23@gmail.com>
2021-06-12 11:37:26 -0300

    articles section

--- src/components/ArticleSection/index.js
+++ src/components/ArticleSection/index.js
@@ -1,18 +1,40 @@
-import { Article } from "./styles/Article";
+import { ArticleDisplay } from "./styles/ArticleSection";
 
 export default function ArticleSection() {
   return (
-    <section>
-      <h2>New to Nuzlockes?</h2>
-      <div>
-        <Article>
-          <img src="/articles/confused-charizard.gif" alt="confused-charizard-gif"/>
+    <ArticleDisplay>
+      <div class="title">
+        <h3>New to Nuzlockes?</h3>
+      </div>
+      <div class="article-panel">
+        <section class="article">
+          <div>
+          <img class="thumbnail" src="/articles/confused-charizard.gif" alt="confused-charizard-gif"/>
+          </div>
           <span>
-            <img src="/light/pokeball.png" alt="nuzlocke-logo"/>
+            {/* <img src="/light/pokeball.png" alt="nuzlocke-logo" width={16}/> */}
             <h4>What is a Nuzlocke?</h4>
           </span>
-        </Article>
+        </section>
+        <section class="article">
+          <div>
+          <img class="thumbnail" src="/articles/champions43.gif" alt="confused-charizard-gif"/>
+          </div>
+          <span>
+            {/* <img  src="/light/pokeball.png" alt="nuzlocke-logo" width={16}/> */}
+            <h4>Start a new run!</h4>
+          </span>
+        </section>
+        <section class="article">
+          <div>
+          <img class="thumbnail" src="/articles/starters43.gif" alt="confused-charizard-gif"/>
+          </div>
+          <span>
+            {/* <img src="/light/pokeball.png" alt="nuzlocke-logo" width={16}/> */}
+            <h4>Check out some stories</h4>
+          </span>
+        </section>
       </div>
-    </section>
+    </ArticleDisplay>
   );
 };
