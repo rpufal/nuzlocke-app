c78fc59cf0e678f6855fec8390d795297c22e2ff
Rafael Pufal <rafael.pufal23@gmail.com>
2021-06-18 02:50:21 -0300

    filter working

--- src/components/GamesSection/index.js
+++ src/components/GamesSection/index.js
@@ -2,7 +2,7 @@ import { GamesDisplay } from "./styles/GamesDisplay"
 import {GamesList} from "../../../services/GamesList";
 import GameCard from "./GameCard";
 import GamesFilter from "../GamesFilter";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function GamesSection() {
   const [filters, setFilters] = useState({
@@ -10,6 +10,36 @@ export default function GamesSection() {
     platforms: "all",
     generations: "all"
   })
+  const [games, setGames] = useState(GamesList['Games']);
+
+  // const filterFunction = (filters) => {
+    
+
+  // }
+
+  useEffect(() => {
+    const gamesFiltered1 = GamesList["Games"].filter((game) => {
+      if (filters["platforms"] !== "all") {
+        return game["Platform"] === filters["platforms"]
+      }
+      return game;
+    });
+    const gamesFiltered2 = gamesFiltered1.filter((game) => {
+      if (filters["generations"] !== "all") {
+        return game["Generation"] === filters["generations"]
+      }
+      return game;
+    });
+    const gamesFiltered3 = gamesFiltered2.filter((game) => {
+      if (filters["unnoficial"]) {
+        return game
+      }
+      return game["Official"] == !filters["unnoficial"]
+    });
+    setGames(gamesFiltered3);
+    console.log(gamesFiltered3);
+  },[filters]);
+
   return(
     <GamesDisplay>
       <div class="title">
@@ -17,7 +47,7 @@ export default function GamesSection() {
       </div>
       <GamesFilter filters={filters} setFilters={setFilters}/>
       <div class="panel">
-        {GamesList["Games"].map((game,index)=> <GameCard game={game} key={index}/>)}
+        {games.map((game,index)=> <GameCard game={game} key={index}/>)}
       </div>
     </GamesDisplay>  
   )
